f4a9bd3b34e20f006ded30db0eb21f2fb74544b2
ChengDian9473 <adamcheng.gl@gmail.com>
2024-12-11 17:03:20 +0800

    fix validation data

--- ChengDianData/v7/create_v7.py
+++ ChengDianData/v7/create_v7.py
@@ -5,8 +5,10 @@ a = int(sys.argv[1])
 b = int(sys.argv[2])
 
 for t in range(a,b):
-    subprocess.call(["rm", "-rf","./datav7_t{}".format(t)])
-    subprocess.call(["mkdir", "./datav7_t{}".format(t)])
+    subprocess.call(["mkdir", "-p", "./datav7_t{}".format(t)])
 
 subprocess.call(["python3", "process_data_v7.py", "{}".format(a), "{}".format(b)])
 subprocess.call(["python3", "process_val_v7.py", "{}".format(a), "{}".format(b)])
+    
+# for t in range(a,b):
+#     subprocess.call(["rm", "-rf","./datav7_t{}".format(t)])
